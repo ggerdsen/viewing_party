3a0f7dd7bf7c3d598aae6efe8b62912944d0c7e6
Garrett Gerdsen <garrett.gerdsen@gmail.com>
2020-10-07 15:16:31 -0600

    Create tests for Welcome page
    Co-authored-by: Christoper Allbritton <63476564+Callbritton@users.noreply.github.com>

--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -12,13 +12,22 @@ RSpec.describe "As an visitor, when I visit the root path, " do
   end
   
   it "I see a button to login" do
+    regular_user = User.create!(name: "Larry", email: "regular_user@email.com", password: "123")
+
     visit "/"
-    within '.topnav' do
-      expect(page).to have_link("Log In")
-    end
+    
+    fill_in :email, with: "#{regular_user.email}"
+    fill_in :password, with: "#{regular_user.password}"
+
+    click_on "Log In"
+    expect(current_path).to eq("/dashboard")
+    expect(page).to have_content("Welcome #{regular_user.name}!")
+    
+    visit "/"
+    expect(page).to_not have_button("Log In")
   end
   
-  it "I see a link to register" do
+  xit "I see a link to register" do
     visit "/"
     within '.topnav' do
       expect(page).to have_link("Register")
