646aef24a5b346dcf2990330164339a56a432c85
Chris Allbritton <cla.inbox@gmail.com>
2020-10-08 10:19:22 -0400

    Refactor handrolled dashboard route to resource and add discover resource routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  
+
   root to: 'welcome#index'
   post "/login", to: "sessions#create"
-  get "/dashboard", to: "dashboard#index"
-  
+  # get "/dashboard", to: "dashboard#index"
+  resources :dashboard, only: :index
+  resources :discover, only: :index
 end
