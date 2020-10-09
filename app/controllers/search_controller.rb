1af6adb679699955de39947fccdf2d064fe38fc2
Chris Allbritton <cla.inbox@gmail.com>
2020-10-09 16:27:13 -0400

    Add env key in place of api key

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,12 +2,10 @@ class SearchController < ApplicationController
   def index
 
     conn = Faraday.new(url: "https://api.themoviedb.org") do |faraday|
-      faraday.params["api_key"] = "01f290a2d631a988135263d0ef1c81e1"
-      # ENV["MOVIE_DB_API_KEY"]
+      faraday.params["api_key"] = ENV["MOVIE_DB_API_KEY"]
     end
-    
+
     response = conn.get("/3/movie/top_rated")
-    binding.pry
 
     json = JSON.parse(response.body, symbolize_names: true)
 
