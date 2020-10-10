f0eec60036fa35814d7654a302f604422ef1bc03
Chris Allbritton <cla.inbox@gmail.com>
2020-10-10 13:47:47 -0400

    Add testing for dashboard spec to confirm creation of friends
    Co-authored-by: Garrett Gerdsen <45319985+ggerdsen@users.noreply.github.com>

--- spec/features/friends/dashboard_spec.rb
+++ spec/features/friends/dashboard_spec.rb
@@ -15,8 +15,23 @@ RSpec.describe "Dashboard" do
       within(".friends") do
         expect(page).to have_field("Friend's Email")
         expect(page).to have_button("Add Friend")
-        expect(page).to have_content("You currently have no friends.")
+        # expect(page).to have_content("You currently have no friends.")
       end
+    end
+
+    it "Can add friends and see those friends in the user dashboard" do
+      visit '/dashboard'
+      friend_user = create(:user)
+
+      within(".friends") do
+        expect(page).to have_content("You currently have no friends.")
+        fill_in :friends_email, with: "#{friend_user.email}"
+        click_on "Add Friend"
+      end
+
+      expect(current_path).to eq('/dashboard')
+      expect(page).to have_content(friend_user.name)
+      save_and_open_page
     end
   end
 end
