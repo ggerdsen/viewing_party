0194c35e1c751f91d6d7483084cc6cb9b2ed3d8b
Garrett Gerdsen <garrett.gerdsen@gmail.com>
2020-10-10 17:48:56 -0600

    Add to gemfile
    Co-authored-by: Christoper Allbritton 
    <63476564+Callbritton@users.noreply.github.com>

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,6 +5,8 @@ require File.expand_path('../config/environment', __dir__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+require 'webmock/rspec'
+require 'vcr'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -62,6 +64,7 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
 end
 
 Shoulda::Matchers.configure do |config|
@@ -69,4 +72,12 @@ Shoulda::Matchers.configure do |config|
     with.test_framework :rspec
     with.library :rails
   end
+end
+
+VCR.configure do |config|
+  config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
+  config.hook_into :webmock
+  config.filter_sensitive_data('<dont_share_my_key>') { ENV['MOVIE_DB_API_KEY'] }
+  config.configure_rspec_metadata!
+  config.default_cassette_options = { re_record_interval: 999999999999999999999999999999999999999999999999999999999999999.days }
 end
