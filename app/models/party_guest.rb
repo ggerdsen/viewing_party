533a619d74d1386c0d91f4e7185e30d827ef2b3c
Chris Allbritton <cla.inbox@gmail.com>
2020-10-13 10:07:24 -0400

    Add relationships and model validations for party and party_guest models

--- app/models/party_guest.rb
+++ app/models/party_guest.rb
@@ -1,4 +1,6 @@
 class PartyGuest < ApplicationRecord
   belongs_to :party
-  belongs_to :guest
+  belongs_to :guest, class_name: 'User'
+
+  validates_presence_of :party_id, :guest_id
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
   has_secure_password
   has_many :friendships, dependent: :destroy
   has_many :friends, through: :friendships
+  has_many :parties, dependent: :destroy
 end
