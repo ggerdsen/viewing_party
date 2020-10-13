3f021cfcd72cf3ffc063ed54cd25e0a4e6996bad
Garrett Gerdsen <garrett.gerdsen@gmail.com>
2020-10-12 18:33:59 -0600

    Create route to destroy Co-authored-by: Christoper Allbritton <63476564+Callbritton@users.noreply.github.com>

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   root to: 'welcome#index'
   post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
 
   resources :dashboard, only: :index
   resources :discover, only: :index
