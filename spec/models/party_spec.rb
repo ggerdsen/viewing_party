6943cca079ae562ca51eb875686f71c934a67b1e
Chris Allbritton <cla.inbox@gmail.com>
2020-10-13 10:07:24 -0400

    Add relationship and validation tests for party and party_guest

--- spec/models/party_spec.rb
+++ spec/models/party_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
-
 RSpec.describe Party, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'relationships' do
+    it { should belong_to :user }
+    it { should have_many :party_guests }
+    it { should have_many(:guests).through(:party_guests) }
+  end
+
+  describe "validations" do
+    it { should validate_presence_of :movie_title }
+    it { should validate_presence_of :party_duration }
+    it { should validate_presence_of :date }
+    it { should validate_presence_of :start_time }
+  end
 end
