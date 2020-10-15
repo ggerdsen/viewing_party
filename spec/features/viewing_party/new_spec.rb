fc08ca0fca6b861d516eb42d73ad0520377d9372
Chris Allbritton <cla.inbox@gmail.com>
2020-10-14 21:37:39 -0400

    Add new_spec test

--- spec/features/viewing_party/new_spec.rb
+++ spec/features/viewing_party/new_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe "Viewing Party New Page" do
       click_on "Log In"
     end
 
-    it "I see a button to create a new viewing party form" do
+    xit "I see a button to create a new viewing party form" do
       VCR.use_cassette("new_viewing_party_form") do
 
         visit "/discover"
@@ -22,6 +22,26 @@ RSpec.describe "Viewing Party New Page" do
         click_on("The Shawshank Redemption")
         click_on("Create Viewing Party for Movie")
         expect(current_path).to eq("/viewing_party/new")
+      end
+    end
+
+    xit "When I click the viewing_party button I am redirected and see a form" do
+      VCR.use_cassette("click_viewing_party_button") do
+
+        visit "/discover"
+        click_on "Find Top Rated Movies"
+
+        expect(current_path).to eq("/movies")
+        expect(page).to have_content("The Shawshank Redemption")
+
+        click_on("The Shawshank Redemption")
+        click_on("Create Viewing Party for Movie")
+        expect(current_path).to eq("/viewing_party/new")
+
+        expect(page).to have_content("The Shawshank Redemption")
+        expect(page).to have_selector("#duration")
+        expect(page).to have_selector("#date")
+        expect(page).to have_selector("#start_time")
       end
     end
   end
