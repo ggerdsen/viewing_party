3307511e88ff96aff8b0dceda6f99aedc813ae1e
Chris Allbritton <cla.inbox@gmail.com>
2020-10-14 21:37:39 -0400

    Add create method for viewing party

--- app/controllers/viewing_party_controller.rb
+++ app/controllers/viewing_party_controller.rb
@@ -1,6 +1,25 @@
 class ViewingPartyController < ApplicationController
   def new
     movie = SearchFacade.new
-    @movie = movie.get_summary(params[:id])
+    @movie = movie.get_summary(params[:movie_id]).first
   end
+
+#   def create
+#     movie = SearchFacade.new
+#     @movie = movie.get_summary(params[:movie_id]).first
+#     @party = Party.new(party_params)
+#     if @party.save
+#       flash[:success] = 'Your viewing party has been created!'
+#       redirect_to '/dashboard'
+#     else
+#       flash[:error] = 'Please try again'
+#       render :new
+#     end
+#   end
+#
+#   private
+#
+#   def party_params
+#     params.permit(:movie_title, :party_duration, :date, :start_time)
+#   end
 end
