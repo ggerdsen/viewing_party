21bc74e6d899091f00e2a39f70af437be637e5f2
Chris Allbritton <cla.inbox@gmail.com>
2020-10-14 21:37:39 -0400

    Add test for creating viewing party event

--- spec/features/viewing_party/new_spec.rb
+++ spec/features/viewing_party/new_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe "Viewing Party New Page" do
       click_on "Log In"
     end
 
-    xit "I see a button to create a new viewing party form" do
+    it "I see a button to create a new viewing party form" do
       VCR.use_cassette("new_viewing_party_form") do
 
         visit "/discover"
@@ -25,7 +25,7 @@ RSpec.describe "Viewing Party New Page" do
       end
     end
 
-    xit "When I click the viewing_party button I am redirected and see a form" do
+    it "When I click the viewing_party button I am redirected and see a form" do
       VCR.use_cassette("click_viewing_party_button") do
 
         visit "/discover"
@@ -39,9 +39,34 @@ RSpec.describe "Viewing Party New Page" do
         expect(current_path).to eq("/viewing_party/new")
 
         expect(page).to have_content("The Shawshank Redemption")
-        expect(page).to have_selector("#duration")
+        expect(page).to have_selector("#party_duration")
         expect(page).to have_selector("#date")
         expect(page).to have_selector("#start_time")
+      end
+    end
+
+    xit "I can fill in the form" do
+      VCR.use_cassette("create_viewing_party") do
+
+        visit "/discover"
+        click_on "Find Top Rated Movies"
+
+        expect(current_path).to eq("/movies")
+        expect(page).to have_content("The Shawshank Redemption")
+
+        click_on("The Shawshank Redemption")
+        click_on("Create Viewing Party for Movie")
+        expect(current_path).to eq("/viewing_party/new")
+
+        expect(page).to have_content("The Shawshank Redemption")
+        expect(page).to have_selector("#party_duration")
+        expect(page).to have_selector("#date")
+        expect(page).to have_selector("#start_time")
+
+        fill_in :party_duration, with: 155
+        fill_in :date, with: "2020/10/14"
+        fill_in :start_time, with: "7:35"
+        click_on("Create Party")
       end
     end
   end
